1a29aee8736445053e0837c8e2a892fbfe0c9fae
adhsu101 <mm@Alexs-MacBook-Pro-2.local>
2014-11-17 19:19:24 -0800

    commit additional files

---
+++ JVAPhotoCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  photoCollectionViewCell.h
+//  Phojo
+//
+//  Created by Mobile Making on 11/17/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVAPhotoCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ JVAPhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  photoCollectionViewCell.m
+//  Phojo
+//
+//  Created by Mobile Making on 11/17/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import "JVAPhotoCollectionViewCell.h"
+
+@implementation JVAPhotoCollectionViewCell
+
+@end
