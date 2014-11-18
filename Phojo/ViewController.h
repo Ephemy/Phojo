d389a8b188dd6631c435e44e399939e38a6fdb84
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-11-17 17:01:22 -0800

    Initial Commit

---
+++ Phojo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phojo
+//
+//  Created by Jonathan Chou on 11/17/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Phojo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Phojo
+//
+//  Created by Jonathan Chou on 11/17/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
